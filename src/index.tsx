15517c9bb7327abed768679a9ef117c5a8e2bbd6
safaley <safalneupane38@gmail.com>
2026-03-25 14:33:29 +0545

    Optimized

--- src/index.tsx
+++ src/index.tsx
@@ -4,24 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const rootElement = document.getElementById('root') as HTMLElement;
-
-// If pre-rendered HTML exists, hydrate instead of full render
-// This preserves the static HTML for instant display while React takes over
-if (rootElement.hasChildNodes()) {
-  ReactDOM.hydrateRoot(
-    rootElement,
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-} else {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 reportWebVitals();
